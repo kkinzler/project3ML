3a30cdbd30cbf34894501ef60cc1993e3a1752a5
kristopher kinzler <kkinzler@pdx.edu>
2019-12-04 12:24:39 -0800

    a barely working k-means

--- kMeans.py
+++ kMeans.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
+import copy
 
 
 #grab the data
@@ -13,78 +14,104 @@ data = np.loadtxt('GMM_data_fall2019.txt')
 
 
 #HYPER PARAMETERS
-kClusters   = 10
-randomStarts = 10
-maxIterations = 5000
+kClusters   = 6
+randomStarts = 5
+maxIterations = 500
+wcStart = 1500.0
 
 dataLength = data.shape[0]
-
-poison = True
+color = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']	
 
 
 def euclidDistance(x1, y1, x2, y2):
 	return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 
-# run the kMeans algorithm with different values for k (eg. k = 2-10)
+#run the kMeans algorithm with different values for k (eg. k = 2-10)
 while kClusters > 1:
 	# grab random data points for centroid of  each cluster
-	randomStarts = 10
-	# maxIter = 1000
-	poison = True	
 	initialCentroids = np.random.randint(low=0, high=dataLength,
 										 size=(randomStarts, kClusters))
-	print("kClusters: ", kClusters)	
-	
-	iterations = 0
-	newCentroidIndices = [[] for i in range(kClusters)]
-	tenRandomSolutions = [[] for i in range(0, 10)]
+#	print("kClusters: ", kClusters)	
+	newCentroids = [[] for i in range(kClusters)]
+	randomStarts = 5 
+	solutions = np.zeros(randomStarts + 1,dtype=float)
+	bestWcss = 1500.0
+	bestSolution = []
+
 	# run 10 times with random initial centroids.
 	while randomStarts > 0:
+		#print("randomStarts: ", randomStarts)
+		#print("iteration: ", iterations)
+		ransomStarts = 5
 		centroidIndices = initialCentroids[randomStarts - 1]
+		print("cIndices: ", centroidIndices)
+		iterations = 0
+		found = False
+
 		#store new value for centroid indices
 		centroids = []
 		for index in centroidIndices:
 			centroids.append(data[index])
-		# List of minimum centroids for each datum (datum index -> centroid index)
-		distances = []
-		for datum in data:
-			centdists = []
-			for centroid in centroids:
-				centdists.append(euclidDistance(datum[0], datum[1], centroid[0], centroid[1]))
-			minindex = centdists.index(min(centdists))
-			#---what's the difference between minindex and centdists[minindex]---
-			distances.append({"datum": datum, "centroidindex": minindex, "distance": centdists[minindex]})
+
+		while found == False: 
+			
+			# List of minimum centroids for each datum (datum index -> centroid index)
+			distances = []
+			for datum in data:
+				centdists = []
+				for centroid in centroids:
+					centdists.append(euclidDistance(datum[0], datum[1], centroid[0], centroid[1]))
+				minindex = centdists.index(min(centdists))
+				distances.append({"datum": datum, "centroidindex": minindex, "distance": centdists[minindex]})
 
 
-		for i in range(kClusters):
-			#store each data point in respective cluster
-			inCluster = [[] for i in range(kClusters)]
-			for row in distances:
-				if row["centroidindex"] == i:
-					inCluster[i].append(row["datum"])
-			#calculate new mean
-			newCentroidIndices[i] = np.mean(inCluster[i], axis=0)
+			for i in range(kClusters):
+				#store each data point in respective cluster
+				inCluster = [[] for i in range(kClusters)]
+				for row in distances:
+					if row["centroidindex"] == i:
+						inCluster[i].append(row["datum"])
+				#calculate new mean
+				newCentroids[i] = np.mean(inCluster[i], axis=0)
+				
+
+			if maxIterations == iterations:
+				for k in range(kClusters):
+					for row in distances:
+						if row["centroidindex"] == k:
+							solutions[k] += row["distance"]
+				wcss = np.sum(solutions)		
+				if wcss < bestWcss:
+					bestSolution = copy.deepcopy(distances)
+					
+				#print("randomStarts: ", randomStarts)
+				#print("randomSolutions:  ", tenRandomSolutions)	
+				randomStarts -= 1
+				found = True
 
 
-		if (centroidIndices == newCentroidIndices) || maxIterations == iterations:
-			stop = True	
-			tenRandomSolutions.append(newCentroidIndices) 
-		else:
-			centroidIndices = newCentroidIndices
-	
-	for row in tenRandomSolutions:
-		for datum in data:
+			else:
+				centroids = copy.deepcopy(newCentroids)
+				"""
+				if iterations % 100 == 0:
+					for i in range(kClusters):
+						for row in distances:
+							if distances["minindex"] == i:
+								pl.scatter(distances["datum"][0],distances["datum"][1],c=color[i],marker='+')
 
-	for i in range(dataLength):
-		if iris.target[i] == 0:
-		c1 = pl.scatter(pca_2d[i,0],pca_2d[i,1],c='r',marker='+')
-		elif iris.target[i] == 1:
-		c2 = pl.scatter(pca_2d[i,0],pca_2d[i,1],c='g',marker='o')
-		elif iris.target[i] == 2:
-		c3 = pl.scatter(pca_2d[i,0],pca_2d[i,1],c='b',marker='*')
-	pl.legend([c1, c2, c3], ['Setosa', 'Versicolor','Virginica'])
-	pl.title('Iris dataset with 3 clusters and known outcomes')
+					pl.title('kmean cluster')
+					pl.show()
+
+				"""	
+				iterations += 1
+
+	for i in range(kClusters):
+		for row in distances:
+			if row["centroidindex"] == i:
+				pl.scatter(row["datum"][0],row["datum"][1],c=color[i],marker='+')
+
+	pl.title('kmean cluster')
 	pl.show()
-
-
+	
+	kClusters -= 1
